a1549cbffeabbb477d74da978ebb5f4008cfaff4
animeshmaiti <a.maiti770@gmail.com>
2023-06-23 11:17:06 +0530

    added search city

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '96db13c95fmsha1d7c0ad90d698dp1f7e0ejsn34ab7ef48860',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
