3f225eeacd2f09a8858b05549dcace0339466f48
animeshmaiti <a.maiti770@gmail.com>
2023-06-24 12:15:16 +0530

    build current weather component

--- src/components/current-weather/current-weather.jsx
+++ src/components/current-weather/current-weather.jsx
@@ -1,11 +1,38 @@
-import './current-weather.css';
+import "./current-weather.css";
 
 const CurrentWeather = () => {
   return (
-    <div className='weather'>
-      <div></div>
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">Kolkata</p>
+          <p className="weather-description">Sunny</p>
+        </div>
+        <img src="icons/01d.png" alt="weather" className="weather-icon" />
+      </div>
+      <div className="bottom">
+        <p className="temperature">18°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Fells like</span>
+            <span className="parameter-value">22°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">4 km/hr</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">15%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">100mb</span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default CurrentWeather;
