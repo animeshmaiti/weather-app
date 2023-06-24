b70d1ac779f2cc46d772d4875d28920df990b28a
animeshmaiti <a.maiti770@gmail.com>
2023-06-24 09:38:44 +0530

    update

---
+++ src/components/current-weather/current-weather.jsx
@@ -0,0 +1,11 @@
+import './current-weather.css';
+
+const CurrentWeather = () => {
+  return (
+    <div className='weather'>
+      <div></div>
+    </div>
+  )
+}
+
+export default CurrentWeather;
