aa721036105132a32171cae2ede03a4b5496d6ea
animeshmaiti <a.maiti770@gmail.com>
2023-06-25 12:32:32 +0530

    updating style

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -3,41 +3,51 @@ import { AsyncPaginate } from "react-select-async-paginate";
 import { geoApiOptions, GEO_API_URL } from "../../api";
 
 const Search = ({ onSearchChange }) => {
-    const [search, setSearch] = useState(null);
+  const [search, setSearch] = useState(null);
 
-    const loadOptions = (inputValue) => {
-        return fetch(
-            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
-            geoApiOptions
-        )
-            .then((response) => response.json())
-            .then((response) => {
-                const options = response.data.map((city) => ({
-                    value: `${city.latitude} ${city.longitude}`,
-                    label: `${city.name}, ${city.country}`,
-                }));
-                return { options };
-            })
-            .catch((error) => {
-                console.error(error);
-                return { options: [] };
-            });
-    };
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        const options = response.data.map((city) => ({
+          value: `${city.latitude} ${city.longitude}`,
+          label: `${city.name}, ${city.country}`,
+        }));
+        return { options };
+      })
+      .catch((error) => {
+        console.error(error);
+        return { options: [] };
+      });
+  };
+  const customStyles = {
+    control: (provided) => ({
+      ...provided,
+      backgroundColor: "#4a4a4a",
+    }),
+    input: (provided) => ({
+      ...provided,
+      color: "white", 
+    }),
+  };
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
 
-    const handleOnChange = (searchData) => {
-        setSearch(searchData);
-        onSearchChange(searchData);
-    };
-
-    return (
-        <AsyncPaginate
-            placeholder="Search for city"
-            debounceTimeout={600}
-            value={search}
-            onChange={handleOnChange}
-            loadOptions={loadOptions}
-        />
-    );
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+      styles={customStyles}
+    />
+  );
 };
 
 export default Search;
